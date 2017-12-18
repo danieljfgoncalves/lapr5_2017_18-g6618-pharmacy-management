c9218185d5eff33e6f0708a0b9c39ccecabdbfc6
Pedro Fernandes <1060503@isep.ipp.pt>
2017-12-18 12:16:40 +0000

    quick fix

--- config.js
+++ config.js
@@ -4,14 +4,9 @@
 module.exports = {
     'secret':'lapr2017',
 
-    //Credentials mlab -> username: arqsi3a | password: Asist2000
-   'medicinesDB':'mongodb://admin:admin@ds044787.mlab.com:44787/lapr5_medicines_g6618',
-    'receiptsDB':'mongodb://admin:admin@ds046067.mlab.com:46067/lapr5_receipts_g6618',
-
     'mongoURI':{
-      'production': 'mongodb://admin:admin@ds141796.mlab.com:41796/lapr5_pharmacies_g6618',
-      'development': 'mongodb://admin:admin@ds141796.mlab.com:41796/lapr5_pharmacies_g6618',
-     // 'test': '' 
+      'development': 'mongodb://admin:admin@ds141796.mlab.com:41796/lapr5-6618-pharmacy-management',
+      'test': 'mongodb://admin:admin@ds141796.mlab.com:41796/lapr5_pharmacies_g6618',
     }
     //API isn´t available
    /* 'medicines_backend':{
