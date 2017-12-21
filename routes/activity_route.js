b9348a063c445295df147918d9a9ce097ad851c9
Pedro Fernandes <1060503@isep.ipp.pt>
2017-12-21 08:47:54 +0000

    quick fix -> activity routes

--- routes/activity_route.js
+++ routes/activity_route.js
@@ -11,15 +11,15 @@ var activityController=require('../controllers/activityController');
 router.use('/activity_route', middlewares.authenticateToken);
 
 // GET /api/pharmacyLog
-router.get('/pharmaciesLogs', activityController.get_logs);
+router.get('/pharmacyLog', activityController.get_logs);
 
 // GET /api/pharmacyLog/{id}
-router.get('/pharmaciesLogs/:id', activityController.get_log);
+router.get('/pharmacyLog/:id', activityController.get_log);
 
 // GET /api/pharmacyLog/date/{dateI}  (dateI ex -> 2017-12-20)
-router.get('/pharmaciesLogs/date/:dateI', activityController.get_log_date);
+router.get('/pharmacyLog/date/:dateI', activityController.get_log_date);
 
 // GET /api/pharmacyLog/since/{dateI}
-router.get('/pharmaciesLogs/since/:dateI/', activityController.get_log_sinceDate);
+router.get('/pharmacyLog/since/:dateI/', activityController.get_log_sinceDate);
 
 module.exports=router;
